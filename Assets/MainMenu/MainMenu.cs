d0ea3d9db564b2d9ee035ab92b47b74173d9ed61
Cyprideux53000 <cyprien.gaudin@supdevinci-edu.fr>
2025-01-14 12:56:02 +0100

    add basic menu

---
+++ Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("1st_level");
+    }
+
+    public void OnOptionsClicked()
+    {
+        Debug.Log("Ouvrir Options");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
