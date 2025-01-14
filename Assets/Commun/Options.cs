0b4d658355a5c3d707faa70497ae275007f2c224
Cyprideux53000 <cyprien.gaudin@supdevinci-edu.fr>
2025-01-14 15:59:45 +0100

    add option menu

--- Assets/Commun/Options.cs
+++ Assets/Commun/Options.cs
@@ -1,50 +1,28 @@
-using System;
-using System.Threading;
+public class Options
+{
+    private static Options _instance;
 
-    // This Singleton implementation is called "double check lock". It is safe
-    // in multithreaded environment and provides lazy initialization for the
-    // Singleton object.
-    class OptionsSingleton
+    public static Options Instance
     {
-        private OptionsSingleton() { }
-
-        private static OptionsSingleton _instance;
-
-        // We now have a lock object that will be used to synchronize threads
-        // during first access to the Singleton.
-        private static readonly object _lock = new object();
-
-        public static OptionsSingleton GetInstance(string value)
+        get
         {
-            // This conditional is needed to prevent threads stumbling over the
-            // lock once the instance is ready.
             if (_instance == null)
-            {
-                // Now, imagine that the program has just been launched. Since
-                // there's no Singleton instance yet, multiple threads can
-                // simultaneously pass the previous conditional and reach this
-                // point almost at the same time. The first of them will acquire
-                // lock and will proceed further, while the rest will wait here.
-                lock (_lock)
-                {
-                    // The first thread to acquire the lock, reaches this
-                    // conditional, goes inside and creates the Singleton
-                    // instance. Once it leaves the lock block, a thread that
-                    // might have been waiting for the lock release may then
-                    // enter this section. But since the Singleton field is
-                    // already initialized, the thread won't create a new
-                    // object.
-                    if (_instance == null)
-                    {
-                        _instance = new OptionsSingleton();
-                        _instance.Color = "#FF000";
-                        _instance.IsBackgroundSoundEnabled = true;
-                    }
-                }
-            }
+                _instance = new Options();
+
             return _instance;
         }
-
-        public string Color { get; set; }
-        public bool IsBackgroundSoundEnabled {get; set;}
     }
+
+    public string Color { get; set; }
+
+    public string test;
+
+    public bool IsBackgroundMusicEnabled { get; set; }
+
+    private Options()
+    {
+        Color = "#FF0000";
+        IsBackgroundMusicEnabled = true;
+        test = "hello";
+    }
+}
--- Assets/OptionMenu/OptionMenu.cs
+++ Assets/OptionMenu/OptionMenu.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class OptionMenu : MonoBehaviour
 {
+    [SerializeField]
+    private string _color;
+    [SerializeField]
+    private bool _backgroundMusicToggle;
+    public void Start()
+    {
+        _backgroundMusicToggle = Options.Instance.IsBackgroundMusicEnabled;
+    }
     public void OnSaveClicked()
     {
-        // Save the options
+        Options.Instance.IsBackgroundMusicEnabled = _backgroundMusicToggle;
         SceneManager.LoadScene(0);
+    }
+
+    public void SetBackgroundMusic(bool value)
+    {
+        _backgroundMusicToggle = value;
     }
 
     public void OnCancelClicked()
