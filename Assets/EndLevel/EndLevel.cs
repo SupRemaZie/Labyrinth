3b81745a8d007bacb255a0c709175e751a7fbc08
Drakiel <tristangautier2003@gmail.com>
2025-01-15 10:14:40 +0100

    Add End_Level With Values

--- Assets/EndLevel/EndLevel.cs
+++ Assets/EndLevel/EndLevel.cs
@@ -5,8 +5,26 @@ using UnityEngine.UI;
 
 public class EndLevel : MonoBehaviour
 {
-    [SerializeField]
-    public string _level;
+    private string _level;
+
+    private string _timer;
+
+    [SerializeField] public TMP_Text _level_text;
+    [SerializeField] public TMP_Text _timer_text;
+
+    void Start()
+    {
+        _timer = PlayerPrefs.GetString("timer");
+        _level = PlayerPrefs.GetString("level");
+
+        setText();
+    }
+
+    private void setText()
+    {
+        _level_text.text = _level + " Finished !";
+        _timer_text.text = "Time : " + _timer;
+    }
 
     public void OnHomeClicked()
     {
